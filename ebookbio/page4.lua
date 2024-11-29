2c33e47318f350073302308c3f49600ca8507ee5
Beatriz <biabiasouza17@gmail.com>
2024-11-29 01:23:18 -0300

    commit
    páginas 4 e 5

--- ebookbio/page4.lua
+++ ebookbio/page4.lua
@@ -1,66 +1,6 @@
 local composer = require("composer")
 local widget = require("widget")
-local physics = require("physics")
 local scene = composer.newScene()
-
--- Física
-physics.start()
-physics.setGravity(0, 9.8)
-
--- Caixas (criar e soltar)
-local function createFallingBoxes()
-    local boxData = { "Animal", "Planta", "Fungo", "Protoctista", "Monera"}
-    local colors = {
-        {0.2, 0.6, 1},
-        {0.3, 0.8, 0.4},
-        {0.2, 0.6, 1},
-        {0.3, 0.8, 0.4},
-        {0.2, 0.6, 1},
-    }
-
-    local startY = 100
-    local delay = 500
-
-    local ground = display.newRect(display.contentCenterX, display.contentHeight - 150, display.contentWidth, 10)
-    ground:setFillColor(0, 0, 0, 0)
-    physics.addBody(ground, "static", { bounce = 0.2 })
-    scene.view:insert(ground)
-
-    for i = 1, #boxData do
-        timer.performWithDelay(delay * i, function()
-        local box = display.newRoundedRect(display.contentCenterX, startY, 200, 70, 12)
-        box:setFillColor(unpack(colors[i]))
-        box.strokeWidth = 2
-        box:setStrokeColor(0)
-
-        local boxText = display.newText({
-            text = boxData[i],
-            x = box.x,
-            y = box.y,
-            font = "Montserrat-VariableFont_wght.ttf-Bold",
-            fontSize = 25,
-        })
-        boxText:setFillColor(1)
-
-        local boxGroup = display.newGroup()
-        boxGroup:insert(box)
-        boxGroup:insert(boxText)
-
-        box.enterFrame = function()
-            boxText.x = box.x
-            boxText.y = box.y
-        end
-        Runtime:addEventListener("enterFrame", box)
-        
-        -- Ativar
-        physics.addBody(box, "dynamic", { density = 1, friction = 0.5, bounce = 0.2})
-        box.angularDamping = 3
-
-        -- Add ao grupo da cena
-        scene.view:insert(boxGroup)
-    end)
-end
-end
 
 -- Som 
 local isSoundOn = true
@@ -71,12 +11,22 @@ end
 
 -- Próxima página
 local function goToNextPage()
-    composer.gotoScene("page4")
+    composer.gotoScene("page5")
 end
 
 -- Página anterior
 local function goToPreviousPage()
-    composer.gotoScene("page2")
+    composer.gotoScene("page3")
+end
+
+-- Função imagem
+local function toggleDogImage(dogImage)
+    if isDefaultImage then
+        dogImage.fill = { type = "image", filename = "Images/ImgPag4/dogalt.png" }
+    else
+        dogImage.fill = { type = "image", filename = "Images/ImgPag4/dog.png" }
+    end
+    isDefaultImage = not isDefaultImage
 end
 
 -- Cena
@@ -90,11 +40,11 @@ function scene:create(event)
     -- Título
     local title = display.newText({
         parent = sceneGroup,
-        text = "Explorando os cinco Reinos",
+        text = "Regras de Nomenclatura",
         x = display.contentCenterX,
         y = 110,
         font = "Montserrat-VariableFont_wght.ttf-Bold",
-        fontSize = 54,
+        fontSize = 56,
         align = "center"
     })
     title:setFillColor(1)
@@ -102,7 +52,7 @@ function scene:create(event)
     -- Explicação
     local explanation = display.newText({
         parent = sceneGroup,
-        text = "A classificação dos seres vivos em cinco reinos foi proposta em 1969 pelo biólogo norte-americano Robert Whittaker. Essa divisão é um tipo de organização que agrupa os seres vivos de acordo com semelhanças entre suas características estruturais, anatômicas e genéticas.\n\nToque no botão amarelo para empilhar as caixas com os nomes dos reinos biológicos.",
+        text = "As regras de nomenclatura garantem que os seres vivos sejam nomeados de forma universal. Exemplo: o nome científico do ser humano é *Homo sapiens*.\n\nToque na imagem para descobrir seu nome científico e como ele é formado.",
         x = display.contentCenterX,
         y = 270,
         width = display.contentWidth - 40,
@@ -112,7 +62,15 @@ function scene:create(event)
     })
     explanation:setFillColor(1)
 
- -- Botão som
+    -- Imagem do cachorro
+    local dogImage = display.newImageRect(sceneGroup, "Images/ImgPag4/dog.png", 600, 600)
+    dogImage.x = display.contentCenterX + 50
+    dogImage.y = display.contentCenterY + 150
+
+    -- Interação imagem
+    dogImage:addEventListener("tap", function() toggleDogImage(dogImage) end)
+
+    -- Botão som
  local soundToggle = widget.newButton({
     label = "Som on/off",
     font = "Montserrat-VariableFont_wght.ttf",
@@ -128,7 +86,7 @@ soundToggle.x = display.contentWidth - 90
 soundToggle.y = display.contentHeight - 60
 sceneGroup:insert(soundToggle)
 
--- Botão "Avançar"
+    -- Botão "Avançar"
 local nextButton = widget.newButton({
     label = "Avançar",
     font = "Montserrat-VariableFont_wght.ttf",
@@ -144,7 +102,7 @@ nextButton.x = display.contentWidth - 90
 nextButton.y = display.contentHeight - 120
 sceneGroup:insert(nextButton)
 
--- Botão "Voltar"
+    -- Botão "Voltar"
 local backButton = widget.newButton({
     label = "Voltar",
     font = "Montserrat-VariableFont_wght.ttf",
@@ -159,8 +117,6 @@ local backButton = widget.newButton({
 backButton.x = 90
 backButton.y = display.contentHeight - 120
 sceneGroup:insert(backButton)
-
-background:addEventListener("tap", createFallingBoxes)
 
 end
 
---
+++ ebookbio/page5.lua
@@ -0,0 +1,208 @@
+local composer = require("composer")
+local widget = require("widget")
+local scene = composer.newScene()
+
+-- Som 
+local isSoundOn = true
+local function toggleSound()
+    isSoundOn = not isSoundOn
+    print("Som:", isSoundOn and "Ligado" or "Desligado")
+end
+
+-- Próxima página
+local function goToNextPage()
+    composer.gotoScene("contracapa")
+end
+
+-- Página anterior
+local function goToPreviousPage()
+    composer.gotoScene("page4")
+end
+
+-- Referências Bibliográficas
+local function showReferences(sceneGroup)
+    local referencesGroup = display.newGroup()
+    sceneGroup:insert(referencesGroup)
+
+    local referencesBackground = display.newRect(referencesGroup, display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
+    referencesBackground:setFillColor(0, 0, 0, 0.8)
+
+    -- Textos
+    local referencesText = display.newText({
+        parent = referencesGroup,
+        text = "TODA MATÉRIA. Reinos dos Seres Vivos. Disponível em: <https://www.todamateria.com.br/reinos-dos-seres-vivos/>. Acesso em: 22 out. 2024.\n\nUOL EDUCAÇÃO. Taxonomia: como funciona o sistema de classificação dos seres vivos. Disponível em: <https://educacao.uol.com.br/disciplinas/biologia/taxonomia-como-funciona-o-sistema-de-classificacao-dos-seres-vivos.htm#:~:text=A%20classifica%C3%A7%C3%A3o%20b%C3%A1sica%20dos%20seres,ordem%2C%20classe%2C%20e%20esp%C3%A9cie>. Acesso em: 22 out. 2024.\n\nEDIÇÕES SM (Org.). Ser protagonista box: biologia, ensino médio: volume único. 1. ed. São Paulo: Edições SM, 2014. (Coleção ser protagonista box).",
+        x = display.contentCenterX,
+        y = display.contentCenterY,
+        width = referencesBackground.width - 60,
+        font = "Montserrat-VariableFont_wght.ttf-Bold",
+        fontSize = 16,
+        align = "justify"
+    })
+    referencesText:setFillColor(1)
+    referencesGroup:insert(referencesText)
+
+    -- Fechar \normal 
+    local closeButton = widget.newButton({
+        label = "Fechar",
+        fontSize = 20,
+        shape = "roundedRect",
+        width = 120,
+        height = 50,
+        fillColor = { default = {1, 0, 0}, over = {0.8, 0, 0} },
+        labelColor = { default = {1}, over = {0.8} },
+        onRelease = function()
+            referencesGroup:removeSelf()
+            referencesGroup = nil
+        end
+    })
+    closeButton.x = display.contentCenterX
+    closeButton.y = display.contentHeight - 80
+    referencesGroup:insert(closeButton)
+end
+
+-- Função lanterna
+local function toggleFlashlight(sceneGroup)
+    local flashlightOn = false
+    local flashlightText
+
+    -- Imagem lanterna /normal
+    local flashlight = display.newImageRect(sceneGroup, "Images/ImgPag5/lanterna.png", 150, 150)
+    flashlight.x = 650
+    flashlight.y = 300
+
+    -- Clique na lanterna
+    local function onFlashlightTapped()
+        flashlightOn = not flashlightOn
+
+        if flashlightOn then
+            flashlight.fill = { type = "image", filename = "Images/ImgPag5/lanterna.png" }
+
+            flashlightText = display.newText({
+                parent = sceneGroup,
+                text = "Conteúdo",
+                x = display.contentCenterX,
+                y = flashlight.y + 120,
+                width = display.contentWidth - 60,
+                font = "Montserrat-VariableFont_wght.ttf",
+                fontSize = 18,
+                align = "center"
+            })
+        else
+            flashlight.fill = { type = "image", filename = "Images/ImgPag5/lanternadesl.png" }
+
+            if flashlightText then
+                flashlightText:removeSelf()
+                flashlightText = nil
+            end
+        end
+    end
+
+    -- Evento lanterna
+    flashlight:addEventListener("tap", onFlashlightTapped)
+end
+
+-- Cena
+function scene:create(event)
+    local sceneGroup = self.view
+
+    -- Fundo
+    local background = display.newRect(sceneGroup, display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
+    background:setFillColor(0.05, 0.2, 0.05)
+
+    -- Título
+    local title = display.newText({
+        parent = sceneGroup,
+        text = "E os Vírus?",
+        x = display.contentCenterX,
+        y = 135,
+        font = "Montserrat-VariableFont_wght.ttf-Bold",
+        fontSize = 70,
+        align = "center"
+    })
+    title:setFillColor(1)
+
+    -- Explicação
+    local explanation = display.newText({
+        parent = sceneGroup,
+        text = "Os vírus não são classificados em nenhum reino.\n\nToque na lanterna para descobrir mais.",
+        x = display.contentCenterX,
+        y = 250,
+        width = display.contentWidth - 60,
+        font = "Montserrat-VariableFont_wght.ttf",
+        fontSize = 20,
+        align = "justify"
+    })
+    explanation:setFillColor(1)
+
+    -- Toggle lanterna
+    toggleFlashlight(sceneGroup)
+
+    -- Botão toggle de som
+    local soundToggle = widget.newButton({
+        label = "Som on/off",
+        font = "Montserrat-VariableFont_wght.ttf",
+        fontSize = 18,
+        shape = "roundedRect",
+        width = 150,
+        height = 40,
+        fillColor = { default = {0.1, 0.5, 0.9}, over = {0.8, 0.8, 0.8} },
+        labelColor = { default = {1}, over = {0} },
+        onRelease = toggleSound
+    })
+    soundToggle.x = display.contentWidth - 90
+    soundToggle.y = display.contentHeight - 60
+    sceneGroup:insert(soundToggle)
+
+    -- Botão "Avançar"
+    local nextButton = widget.newButton({
+        label = "Avançar",
+        font = "Montserrat-VariableFont_wght.ttf",
+        fontSize = 18,
+        shape = "roundedRect",
+        width = 150,
+        height = 40,
+        fillColor = { default = {0.9, 0.9, 0.9}, over = {0.8, 0.8, 0.8} },
+        labelColor = { default = {0}, over = {0.5} },
+        onRelease = goToNextPage
+    })
+    nextButton.x = display.contentWidth - 90
+    nextButton.y = display.contentHeight - 120
+    sceneGroup:insert(nextButton)
+
+    -- Botão "Voltar"
+    local backButton = widget.newButton({
+        label = "Voltar",
+        font = "Montserrat-VariableFont_wght.ttf",
+        fontSize = 18,
+        shape = "roundedRect",
+        width = 150,
+        height = 40,
+        fillColor = { default = {0.9, 0.9, 0.9}, over = {0.8, 0.8, 0.8} },
+        labelColor = { default = {0}, over = {0.5} },
+        onRelease = goToPreviousPage
+    })
+    backButton.x = 90
+    backButton.y = display.contentHeight - 120
+    sceneGroup:insert(backButton)
+
+    -- Botão "Referências Bibliográficas"
+    local referencesButton = widget.newButton({
+        label = "Referências Bibliográficas",
+        font = "Montserrat-VariableFont_wght.ttf",
+        fontSize = 18,
+        shape = "roundedRect",
+        width = 250,
+        height = 50,
+        fillColor = { default = {1, 0.8, 0}, over = {1, 0.9, 0.5} },
+        labelColor = { default = {0}, over = {0} },
+        onRelease = function() showReferences(sceneGroup) end
+    })
+    referencesButton.x = display.contentCenterX
+    referencesButton.y = display.contentHeight - 130
+    sceneGroup:insert(referencesButton)
+
+end
+
+scene:addEventListener("create", scene)
+
+return scene
